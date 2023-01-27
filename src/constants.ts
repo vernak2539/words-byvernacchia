031c861322a02b713eafc7222ed67f52d496cef3
Alex Vernacchia <alex.vernacchia@deliveroo.co.uk>
2023-01-27 17:40:40 +0000

    blog coming soon

--- src/constants.ts
+++ src/constants.ts
@@ -1,4 +1,9 @@
-export const SITE_TITLE_SPLIT = ["Words by", "Vernacchia"];
-export const SITE_TITLE = SITE_TITLE_SPLIT.join(" ");
+export const BLOG_SITE_TITLE_SPLIT = ["Words by", "Vernacchia"];
+export const HOME_SITE_TITLE_SPLIT = ["by", "Vernacchia"];
 export const SITE_DESCRIPTION =
     "They may not always be right, but they're words...";
+
+export const SITE_TITLES_SPLIT = {
+    home: HOME_SITE_TITLE_SPLIT,
+    blog: BLOG_SITE_TITLE_SPLIT,
+};
