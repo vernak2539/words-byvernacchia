b26bb1de1adeeb249331918c98cc1300d0746751
Alex Vernacchia <alex.vernacchia@deliveroo.co.uk>
2023-01-27 16:43:39 +0000

    customise homepage

--- src/constants.ts
+++ src/constants.ts
@@ -1,3 +1,4 @@
-export const SITE_TITLE = "Words by Vernacchia";
+export const SITE_TITLE_SPLIT = ["Words by", "Vernacchia"];
+export const SITE_TITLE = SITE_TITLE_SPLIT.join(" ");
 export const SITE_DESCRIPTION =
     "They may not always be right, but they're words...";
