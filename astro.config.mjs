b8eb1e991496dff16fb19516fd755b73d912be8e
Alex Vernacchia <alvernacchia@gmail.com>
2023-02-19 21:11:45 +0000

    add sitemap integration

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,6 +1,10 @@
 import { defineConfig } from 'astro/config';
 
 // https://astro.build/config
+import sitemap from "@astrojs/sitemap";
+
+// https://astro.build/config
 export default defineConfig({
-    site: 'https://words.byvernacchia.com',
+  site: 'https://words.byvernacchia.com',
+  integrations: [sitemap()]
 });
