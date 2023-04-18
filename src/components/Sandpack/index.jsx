9493aad2d6550f9484db689eea60005fe3da8384
Alex Vernacchia <alex.vernacchia@deliveroo.co.uk>
2023-04-18 17:40:53 +0100

    sandpack wip

---
+++ src/components/Sandpack/index.jsx
@@ -0,0 +1,10 @@
+import { Sandpack } from "@codesandbox/sandpack-react";
+
+const App = () => {
+  return <Sandpack theme="light" options={{
+    showConsole: true,
+    // showConsoleButton: true,
+  }} />
+}
+
+export default App
