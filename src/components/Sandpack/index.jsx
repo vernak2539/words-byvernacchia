64e637ba7f512161f5b650a9fed6e70cdd1a98fd
Alex Vernacchia <alex.vernacchia@deliveroo.co.uk>
2023-04-19 15:01:22 +0100

    more js event text

--- src/components/Sandpack/index.jsx
+++ src/components/Sandpack/index.jsx
@@ -1,10 +1,15 @@
 import { Sandpack } from "@codesandbox/sandpack-react";
 
 const App = () => {
-  return <Sandpack theme="light" options={{
-    showConsole: true,
-    // showConsoleButton: true,
-  }} />
-}
+    return (
+        <Sandpack
+            theme="light"
+            options={{
+                showConsole: true,
+                // showConsoleButton: true,
+            }}
+        />
+    );
+};
 
-export default App
+export default App;
