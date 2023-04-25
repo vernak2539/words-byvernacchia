6a49609dd088bbd40981453c6887e1a6c54fb2a4
Alex Vernacchia <alex.vernacchia@deliveroo.co.uk>
2023-04-25 16:04:18 +0100

    more content

--- src/content/blog/2023/04/_simulating-js-events-code.js
+++ src/content/blog/2023/04/_simulating-js-events-code.js
@@ -151,3 +151,182 @@ button.addEventListener("mousedown", () => {
         },
     },
 };
+
+const keyboardEventsHTML = `<!DOCTYPE html>
+<html>
+  <head>
+    <title>Simulating JS Events</title>
+    <meta charset="UTF-8" />
+  </head>
+
+  <body>
+    <input id="simulate-keyboard-input" />
+    <br /><br />
+    <script src="index.js"></script>
+  </body>
+</html>
+`;
+
+export const keyboardEventsInputOnly = {
+    files: {
+        "index.html": {
+            readOnly: true,
+            hidden: true,
+            code: keyboardEventsHTML,
+        },
+        "/index.js": {
+            readOnly: true,
+            active: true,
+            code: `const input = document.getElementById("simulate-keyboard-input");
+
+input.addEventListener("keydown", (e) => {
+  console.log("keydown event: " + e.key);
+});
+
+input.addEventListener("keyup", (e) => {
+  console.log("keyup event: " + e.key);
+});
+
+input.addEventListener("keypress", (e) => {
+  console.log("keypress event: " + e.key);
+});
+            `,
+        },
+    },
+};
+
+export const keyboardEventsDispatchEvents = {
+    files: {
+        "index.html": {
+            readOnly: true,
+            hidden: true,
+            code: `<!DOCTYPE html>
+<html>
+  <head>
+    <title>Simulating JS Events</title>
+    <meta charset="UTF-8" />
+  </head>
+
+  <body>
+    <input id="simulate-keyboard-input" />
+    <br /><br />
+    <button id="simulate-keyboard-btn">Simulate typing of "e"</button>
+    <script src="index.js"></script>
+  </body>
+</html>
+            `,
+        },
+        "/index.js": {
+            readOnly: true,
+            active: true,
+            code: `const input = document.getElementById("simulate-keyboard-input");
+const btn = document.getElementById("simulate-keyboard-btn");
+
+input.addEventListener("keydown", (e) => {
+  console.log("keydown event: " + e.key);
+});
+
+input.addEventListener("keyup", (e) => {
+  console.log("keyup event: " + e.key);
+});
+
+input.addEventListener("keypress", (e) => {
+  console.log("keypress event: " + e.key);
+});
+
+btn.addEventListener('click', (e) => {
+    input.dispatchEvent(
+        new KeyboardEvent('keydown', {
+            view: window,
+            bubbles: true,
+            cancelable: true,
+            key: "e",
+            keyCode: 69,
+            code: "KeyE",
+            which: 69,
+        })
+    )
+    
+    input.dispatchEvent(
+        new KeyboardEvent('keypress', {
+            view: window,
+            bubbles: true,
+            cancelable: true,
+            key: "e",
+            keyCode: 69,
+            code: "KeyE",
+            which: 69,
+        })
+    )
+    
+    input.dispatchEvent(
+        new KeyboardEvent('keyup', {
+            view: window,
+            bubbles: true,
+            cancelable: true,
+            key: "e",
+            keyCode: 69,
+            code: "KeyE",
+            which: 69,
+        })
+    )
+})
+            `,
+        },
+    },
+};
+
+export const keyboardEventsInputDotValue = {
+    files: {
+        "index.html": {
+            readOnly: true,
+            hidden: true,
+            code: `<!DOCTYPE html>
+<html>
+  <head>
+    <title>Simulating JS Events</title>
+    <meta charset="UTF-8" />
+  </head>
+
+  <body>
+    <input id="simulate-keyboard-input" />
+    <br /><br />
+    <button id="simulate-keyboard-btn">Simulate typing of "test me"</button>
+    <script src="index.js"></script>
+  </body>
+</html>
+            `,
+        },
+        "/index.js": {
+            readOnly: true,
+            active: true,
+            code: `const input = document.getElementById("simulate-keyboard-input");
+const btn = document.getElementById("simulate-keyboard-btn");
+
+function insertChars(inputElement, string) {
+  inputElement.value += string.charAt(0);
+
+  setTimeout(function() {
+    insertChars(inputElement, string.slice(1));
+  }, 300);
+}
+
+btn.addEventListener('click', (e) => {
+    insertChars(input, "test me");
+})
+
+input.addEventListener("keydown", (e) => {
+  console.log("keydown event: " + e.key);
+});
+
+input.addEventListener("keyup", (e) => {
+  console.log("keyup event: " + e.key);
+});
+
+input.addEventListener("keypress", (e) => {
+  console.log("keypress event: " + e.key);
+});
+            `,
+        },
+    },
+};
