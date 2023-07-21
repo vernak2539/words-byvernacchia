9aa68f7f2a50bf7f3b260914360060dbcde0fef2
Alex Vernacchia <alex.vernacchia@deliveroo.co.uk>
2023-07-21 14:29:11 +0100

    update prettier + format files

--- src/utils/sort-posts.ts
+++ src/utils/sort-posts.ts
@@ -2,5 +2,5 @@ import type { CollectionEntry } from "astro:content";
 
 export const sortByPubdateMostRecentFirst = (
     a: CollectionEntry<"blog">,
-    b: CollectionEntry<"blog">
+    b: CollectionEntry<"blog">,
 ) => b.data.pubDate.valueOf() - a.data.pubDate.valueOf();
