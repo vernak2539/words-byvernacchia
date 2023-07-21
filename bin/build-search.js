0638129845d33d874f33767ecf30176435cbfd4d
Alex Vernacchia <alex.vernacchia@deliveroo.co.uk>
2023-07-21 14:42:22 +0100

    fix mdx files in search

--- bin/build-search.js
+++ bin/build-search.js
@@ -6,45 +6,48 @@ import fs from "fs";
 import path from "path";
 import matter from "gray-matter";
 import removeMd from "remove-markdown";
-import {globSync} from 'glob'
+import { globSync } from "glob";
 
 const client = algoliasearch(
     process.env.ALGOLIA_APP_ID,
-    process.env.ALGOLIA_WRITE_API_KEY
+    process.env.ALGOLIA_WRITE_API_KEY,
 );
 
 const pathToBlog = "./src/content/blog/**/**.{md,mdx}";
 const validPathRegex = /^src\/content\/blog\/\d{4}\/\d{2}\/.*$/;
 
 const getPathFromFileName = (filename) => {
-    const parts = filename.split('/');
+    const parts = filename.split("/");
     const year = parts[3];
     const month = parts[4];
-    const postName = parts[5].slice(0, -3); // Remove ".md" extension
+    const postName = parts[5].split(".")[0];
+
     return `/blog/${year}/${month}/${postName}`;
-}
+};
 
 const filenames = globSync(path.join(pathToBlog));
-const data = filenames.map((filename) => {
-    if(!validPathRegex.test(filename)) {
-        return
-    }
+const data = filenames
+    .map((filename) => {
+        if (!validPathRegex.test(filename)) {
+            return;
+        }
 
-    try {
-        const markdownWithMeta = fs.readFileSync(filename);
-        const { data: frontmatter, content } = matter(markdownWithMeta);
-        return {
-            objectID: path.parse(filename).name,
-            title: frontmatter.title,
-            description: frontmatter.description,
-            urlPath: getPathFromFileName(filename),
-            tags: frontmatter.tags,
-            content: removeMd(content).replace(/\n/g, ""),
-        };
-    } catch (e) {
-        // console.log(e.message)
-    }
-}).filter(Boolean);
+        try {
+            const markdownWithMeta = fs.readFileSync(filename);
+            const { data: frontmatter, content } = matter(markdownWithMeta);
+            return {
+                objectID: path.parse(filename).name,
+                title: frontmatter.title,
+                description: frontmatter.description,
+                urlPath: getPathFromFileName(filename),
+                tags: frontmatter.tags,
+                content: removeMd(content).replace(/\n/g, ""),
+            };
+        } catch (e) {
+            // console.log(e.message)
+        }
+    })
+    .filter(Boolean);
 
 console.log(data);
 
