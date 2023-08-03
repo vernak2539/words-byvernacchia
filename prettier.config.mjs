662a1c099dbf5965bcc2f159441d7d84da94d3b5
Alex Vernacchia <alex.vernacchia@deliveroo.co.uk>
2023-08-03 17:01:45 +0100

    update prettier integration

---
+++ prettier.config.mjs
@@ -0,0 +1,15 @@
+import * as prettierAstro from 'prettier-plugin-astro'
+
+export default {
+  plugins: [prettierAstro],
+  overrides: [
+    {
+      files: '*.astro',
+      options: {
+        parser: 'astro',
+      },
+    },
+  ],
+  "tabWidth": 4,
+  "useTabs": false
+}
